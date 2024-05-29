85c8309c8b76661b22000b1576df1054e3180ddb
tienhung-ho <hohung1203@gmail.com>
2024-05-29 16:41:16 +0700

    Add func creation

--- main.go
+++ main.go
@@ -22,6 +22,15 @@ type TodoItem struct {
 	Updated_at  *time.Time `json:"updated_at"`
 }
 
+type TodoItemCreation struct {
+	Id          int    `json:"-" gorm:"column:id;"`
+	Title       string `json:"title" gorm:"column:title;"`
+	Description string `json:"description" gorm:"column:description;"`
+	Status      string `json:"status" gorm:"column:status;"`
+}
+
+func (TodoItemCreation) TableName() string { return "todo_items" }
+
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
@@ -43,10 +52,53 @@ func main() {
 
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	// CRUD: Create, Read, Update, Delete
+	// /v1/
+
+	v1 := r.Group("/v1")
+	{
+		items := v1.Group("/items")
+		{
+			items.POST("", CreateItem(db))
+			items.GET("")
+			items.GET("/:id")
+			items.PATCH("/:id")
+			items.DELETE("/:id")
+		}
+	}
+
+	r.GET("/v1/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"data": "ok",
 		})
 	})
 	r.Run(":3000") // listen and serve on 0.0.0.0:3000 (for windows "localhost:3000")
+}
+
+func CreateItem(db *gorm.DB) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItemCreation
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if data.Status == "" {
+			data.Status = "Doing"
+		}
+
+		if err := db.Create(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data.Id,
+		})
+
+	}
 }
